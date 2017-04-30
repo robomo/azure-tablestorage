c5f152eb49d26dcba4f08ffa37922c84b9e00748
Robert Mohri <robert.mohri@gmail.com>
2017-04-30 10:06:28 +0200

    adding files

---
+++ TableStorage/Model/SensorReadingEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableStorage.Model
+{
+
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    public class SensorReadingEntity : TableEntity
+    {
+
+        public SensorReadingEntity() { }
+
+        public SensorReadingEntity(string sensorId)
+        {
+            PartitionKey = sensorId;
+            //RowKey = (DateTime.MaxValue.Ticks - timeOfReport.Ticks).ToString("d19");
+            RowKey = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+        }
+
+        public Double Temperature { get; set; }
+        public Double Humidity { get; set; }
+        public Double Light { get; set; }
+
+    }
+
+}
